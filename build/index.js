0609a607f3af78fce59fc9855548dc7e7b6e7f1c
gleb-mikhalkov <gmihalkov123@gmail.com>
2020-05-29 21:50:46 +0300

    Фикс экспорта библиотеки

--- build/index.js
+++ build/index.js
@@ -10,9 +10,9 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("Service"), exports);
-__exportStar(require("LazyService"), exports);
-__exportStar(require("StrictService"), exports);
-__exportStar(require("NewNotAllowedError"), exports);
-__exportStar(require("NotInitializedError"), exports);
-__exportStar(require("UndefinedInstanceError"), exports);
+__exportStar(require("./Service"), exports);
+__exportStar(require("./LazyService"), exports);
+__exportStar(require("./StrictService"), exports);
+__exportStar(require("./NewNotAllowedError"), exports);
+__exportStar(require("./NotInitializedError"), exports);
+__exportStar(require("./UndefinedInstanceError"), exports);
